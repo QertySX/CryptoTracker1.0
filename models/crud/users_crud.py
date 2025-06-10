865f1b70a7567036d8483b5f8ce68bc8d6b4de23
ArtemHirus <artem.girus212@gmail.com>
2025-06-10 22:05:35 +0300

    Создан отдельный файл для работы с таблицей users

---
+++ models/crud/users_crud.py
@@ -0,0 +1,17 @@
+
+
+class AddUserCommand:
+    def __init__(self, session, username, email, password_hash):
+        self.session = session
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+
+    async def execute(self):
+        new_user = User(
+            username = self.username,
+            email = self.email,
+            password_hash = self.password_hash
+        )
+        self.session.add(new_user)
+        await self.session.commit()
