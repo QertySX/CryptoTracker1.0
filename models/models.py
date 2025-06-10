918c3a482962f23737a2fa188120ecfe1df1401f
ArtemHirus <artem.girus212@gmail.com>
2025-06-10 18:07:17 +0300

    Создана база данных

---
+++ models/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_NAME = 'CryptoTraker-App.sqlite'
+
+
+engine = create_engine(f'sqlite:///{DATABASE_NAME}')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+def create_db():
+    Base.metadata.create_all(engine)
+    print('DB created')
+
+
+create_db()
