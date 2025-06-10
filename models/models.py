06d5e93e96e52786cead3a2239d5c5d6d0675bf0
ArtemHirus <artem.girus212@gmail.com>
2025-06-10 21:11:58 +0300

    Написал модели sqlalchemy

--- models/models.py
+++ models/models.py
@@ -1,19 +1,53 @@
-from sqlalchemy import create_engine
+from sqlalchemy import String, Integer, Float, ForeignKey, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-
-DATABASE_NAME = 'CryptoTraker-App.sqlite'
-
-
-engine = create_engine(f'sqlite:///{DATABASE_NAME}')
-Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-def create_db():
-    Base.metadata.create_all(engine)
-    print('DB created')
+# 1. Users
+class User(Base):
+    __tablename__ = "Users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+
+    # Связи
+    portfolio: Mapped["Portfolio"] = relationship(back_populates="user", uselist=False)
+    transactions: Mapped[list["Transaction"]] = relationship(back_populates="user")
 
 
-create_db()
+# 2. Portfolio
+class Portfolio(Base):
+    __tablename__ = "Portfolio"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    crypto_currency: Mapped[str] = mapped_column(Text, nullable=False)
+    buy_price: Mapped[float] = mapped_column(Float, nullable=False)
+    total_amount: Mapped[float] = mapped_column(Float, nullable=False)
+    average_buy_price: Mapped[float] = mapped_column(Float, nullable=False)
+    date: Mapped[str] = mapped_column(String, nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"), unique=True, nullable=False)
+
+    # Связь
+    user: Mapped["User"] = relationship(back_populates="portfolio")
+
+
+# 3. Transactions
+class Transaction(Base):
+    __tablename__ = "Transactions"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    crypto_currency: Mapped[str] = mapped_column(Text, nullable=False)
+    quantity: Mapped[float] = mapped_column(Float, nullable=False)
+    transaction_date: Mapped[str] = mapped_column(String, nullable=False)
+    transaction_type: Mapped[str] = mapped_column(String, nullable=False)
+    price_per_unit: Mapped[float] = mapped_column(Float, nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"), nullable=False)
+
+    # Связь
+    user: Mapped["User"] = relationship(back_populates="transactions")
+
