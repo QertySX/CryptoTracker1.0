fde7f45a6492697e1beaf9f3ab6c70323f0c5d0e
ArtemHirus <artem.girus212@gmail.com>
2025-06-27 14:32:36 +0300

    Добавлен middleware

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi.routing import APIRouter
-from routers.crypto import crypto_router
+from fastapi.responses import RedirectResponse
+from routers.index import home_router
+from routers.auth import auth_router
+from routers.register import reg_router
+from routers.portfolio import portfolio_route
 
 
 app = FastAPI()
 
+@app.middleware("http")
+async def dispatch(request: Request, call_next):
+    try:
+        response = await call_next(request)
+        # Если пользователь зашёл на защищённый путь и не авторизован
+        if request.url.path.startswith('/portfolio') and response.status_code == 401:
+            return RedirectResponse(url="/login")
+        return response
+    except HTTPException as exc:
+        if request.url.path.startswith('/portfolio') and exc.status_code == 401:
+            return RedirectResponse(url="/login")
+        raise exc
+
+
 templates = Jinja2Templates(directory="templates")
 
-app.include_router(crypto_router, prefix="")
-app.include_router(crypto_router, prefix="/search", tags=["search"])
+app.include_router(home_router, prefix="")
+app.include_router(home_router, tags=["search"])
+app.include_router(reg_router, prefix='')
+app.include_router(portfolio_route, prefix='')
+app.include_router(auth_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8010 , reload=True)
