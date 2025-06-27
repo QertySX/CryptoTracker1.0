cb30ff00ac8840be6d5a763ab1c25fcc8845b629
ArtemHirus <artem.girus212@gmail.com>
2025-06-27 14:27:21 +0300

    Написана логика авторизации и аутентификации

--- routers/auth.py
+++ routers/auth.py
@@ -1,16 +1,33 @@
-from fastapi import APIRouter, Request
-from crud.users_crud.py import AddUserCommand
+from fastapi import APIRouter, Request, Response, HTTPException, Depends
 from fastapi.templating import Jinja2Templates
+from models.form import GetForm
+from services.auth_logic import authenticate_user, create_access_token
+from fastapi.responses import RedirectResponse
+from config import ACCESS_TOKEN_EXPIRE
 
 
+auth_router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-reg_router = APIRouter()
 
-@app.get
-@reg_router.get('register')
-async def register_users():
-    ''' 1. Дописать параметры с pydantic
-        2. Доделать регистрацию 
-        3. Захешировать пароль
-    '''
-    pass
+
+@auth_router.get('/login', name='auth')
+async def show_login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@auth_router.post("/login")
+async def login_process(response: Response, user: GetForm = Depends(GetForm.login_form)):
+    user = await authenticate_user(user.username, user.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    token = create_access_token(data={"sub": user.username})
+    response = RedirectResponse(url="/home", status_code=302)
+
+    response.set_cookie(
+        key="access_token", 
+        value=f"Bearer {token}", 
+        httponly=True, 
+        max_age=ACCESS_TOKEN_EXPIRE)
+    return  response
+
