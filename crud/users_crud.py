597268c77853875628304605e455f5b6bc5a1136
ArtemHirus <artem.girus212@gmail.com>
2025-06-27 14:24:20 +0300

    Не значительные изменения

--- crud/users_crud.py
+++ crud/users_crud.py
@@ -1,22 +1,28 @@
-from command_templates import Command
+from create_db import async_session
+from models.models import User
+import bcrypt
 
-'''Доработать все crud-методы на основе паттерна Command'''
 
-class AddUserCommand(Command):
-    def __init__(self, session, username, email, password_hash):
-        self.session = session
+
+class AddUser:
+    def __init__(self, username, email, password_hash):
         self.username = username
         self.email = email
         self.password_hash = password_hash
+        
 
     async def execute(self):
-        new_user = User(
-            username = self.username,
-            email = self.email,
-            password_hash = self.password_hash
+        hashed_password = bcrypt.hashpw(
+            self.password_hash.encode('utf-8'), 
+            bcrypt.gensalt()
         )
-        self.session.add(new_user)
-        await self.session.commit()
-        return new_user
-
         
+        async with async_session() as session:
+            new_user = User(
+                username = self.username,
+                email = self.email,
+                password_hash = hashed_password
+            )
+            session.add(new_user)
+            await session.commit()
+            return new_user
