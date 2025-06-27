7e86dbb1c4eb7f3e4142375022ab2dbe44a49400
ArtemHirus <artem.girus212@gmail.com>
2025-06-27 14:29:14 +0300

    Дробавлены модели для авторизации и аутентификации и модель для портфолио

--- models/schemas.py
+++ models/schemas.py
@@ -1,8 +1,45 @@
-from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
+from pydantic import BaseModel, EmailStr
+from fastapi.security import OAuth2PasswordBearer
+
 
 class UsersRegistration(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
 
-    username = str
-    email = EmailStr
-    password_hash = str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class UserS(BaseModel):
+    username: str
+    email: str | None = None
+    disabled: bool | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class AddCrypto(BaseModel):
+    currency: str
+    price: float
+    total_amount: float
+
+
+
+
+
+
+
+
+        
