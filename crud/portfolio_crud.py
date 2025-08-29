570954eca5904f9c46121b72b84823f1c9ca980e
ArtemHirus <artem.girus212@gmail.com>
2025-08-29 18:11:33 +0300

    Видалено зайві імпорти

--- crud/portfolio_crud.py
+++ crud/portfolio_crud.py
@@ -1,5 +1,10 @@
 from create_db import async_session
 from models.models import Portfolio, User
+import logging
+from sqlalchemy import func, delete, update
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 class AddCryptoInDb:
@@ -24,4 +29,43 @@ class AddCryptoInDb:
                 await session.commit()
                 return crypto_data
         except Exception as e:
-            print(f'[INFO] Ошибка {e}')
+            logging.error(f'[INFO] Ошибка {e}')
+
+
+class DeleteCryptoInDb:
+    def __init__(self, crypto_currency: str, user_id: int):
+        self.crypto_currency = crypto_currency
+        self.user_id = user_id
+
+    async def execute(self):
+        try:
+            async with async_session() as session:
+                delete_data = delete(Portfolio).where(
+                    func.lower(Portfolio.crypto_currency) == self.crypto_currency.lower(),
+                    Portfolio.user_id == self.user_id
+                )
+                await session.execute(delete_data)
+                await session.commit()
+        except Exception as e:
+            logging.error(f'[INFO] Ошибка при удалении криптовалюты: {e}')
+
+
+class EditCryptoInDb:
+    def __init__(self, crypto_currency: str, user_id: int, total_amount: float, buy_price: float):
+        self.crypto_currency = crypto_currency
+        self.total_amount = total_amount
+        self.user_id = user_id
+        self.buy_price = buy_price
+
+    async def execute(self):
+        try:
+            async with async_session() as session:
+                edit_crypto = (
+                    update(Portfolio)
+                    .where(func.lower(Portfolio.crypto_currency) == self.crypto_currency.lower(), Portfolio.user_id == self.user_id)
+                    .values(total_amount=self.total_amount, buy_price=self.buy_price))
+
+                await session.execute(edit_crypto)
+                await session.commit()
+        except Exception as e:
+            logging.error(f'[INFO] Ошибка при удалении криптовалюты: {e}')
