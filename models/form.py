e7ec1839f9616f0932cf4f3138b4ec116be9518a
ArtemHirus <artem.girus212@gmail.com>
2025-08-29 18:11:08 +0300

    Додано форму для отримання даних про змінення та видалення активу в портфелі

--- models/form.py
+++ models/form.py
@@ -1,12 +1,11 @@
 from fastapi import Form
-from models.schemas import UsersRegistration, UserLogin, AddCrypto
+from models.schemas import UsersRegistration, UserLogin, AddCrypto, EditCrypto, DeleteCrypto
 
 
 class GetForm:
     @staticmethod
     def register_form(username: str = Form(...),email: str = Form(...),password: str = Form(...)):
         return UsersRegistration(username=username, email=email, password=password)
-
 
     @classmethod
     def login_form(self, username: str = Form(...), password: str = Form(...)):
@@ -17,3 +16,15 @@ class GetPortfolioForm:
     @staticmethod
     def get_crypto_data(currency = Form(...), price = Form(...), total_amount = Form(...), date = Form(...)):
         return AddCrypto(currency=currency, price=price, total_amount=total_amount, date=date)
+
+
+class DeleteCryptoForm:
+    @staticmethod
+    def delete_crypto(crypto_currency = Form(...)):
+        return DeleteCrypto(crypto_currency=crypto_currency)
+
+
+class EditCryptoForm:
+    @staticmethod
+    def edit_crypto(crypto_currency = Form(...), total_amount = Form(...), buy_price = Form(...)):
+        return EditCrypto(crypto_currency=crypto_currency, total_amount=total_amount, buy_price=buy_price)
